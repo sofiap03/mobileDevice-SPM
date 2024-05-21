09e31a5100705ef0fa4bc82c6956a6d98981e95f
sofia <sofia.pinuelam@autonoma.edu.co>
2024-05-21 16:54:32 -0500

    Feature: Create the new screens and routing file

---
+++ src/screens/Screen3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Screen3 = () => {
+    return(
+        <Text>Screen3</Text>
+    )
+}
---
+++ src/screens/Screen4.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const Screen4 = () => {
+    return(
+        <Text>Screen4</Text>
+    )
+}
